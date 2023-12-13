451dba73ef1a42180db9048c06ad3f34995db693
Denys Savin <dxnsav@gmail.com>
2023-12-13 21:33:49 +0100

    Add Card component to MovieDetails and create Card UI components

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -16,6 +16,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { AddToWatchListButton } from "./AddToWatchListButton";
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
+import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
 import { ScrollArea } from "./ui/scroll-area";
 
 const MovieDetails = () => {
@@ -160,7 +161,7 @@ const MovieDetails = () => {
 				</div>
 			</div>
 
-			<div className="flex flex-row m-6 justify-between">
+			<div className="flex flex-row m-6 justify-between gap-2">
 				<div className="flex flex-col w-full gap-3">
 					<div className="flex flex-row gap-2">
 						<NewMovieTag />
@@ -188,7 +189,7 @@ const MovieDetails = () => {
 			<div className="flex flex-col gap-4 m-6">
 				<h3 className="text-lg font-semibold">Схожі</h3>
 				<ScrollArea className="w-full rounded-md pr-4 h-[55vh]">
-					<div className="grid grid-cols-1 md:grid-cols-2 landscape:grid-cols-3 gap-4 overflow-x-auto">
+					<div className="grid xs:grid-cols-1 sm:grid-cols-2 landscape:grid-cols-3 gap-4 overflow-x-auto">
 						{[...Array(10)].map((_, index) => (
 							<MovieDetailsCard
 								ageRating="16+"
@@ -291,22 +292,37 @@ const DetailsBlock = ({ content, isActors, onMoreClick, title }) => {
 	);
 };
 
-const MovieDetailsCard = ({
-	ageRating,
-	description,
-	duration,
-	matchPercentage,
-	seasons,
-	title,
-	year,
-}) => {
+const MovieDetailsCard = (movie) => {
+	const {
+		ageRating,
+		backdrop_url,
+		description,
+		duration,
+		isAdded,
+		matchPercentage,
+		movie_id,
+		releaseYear,
+		seasons,
+		title,
+	} = movie;
+	const navigate = useNavigate();
+
+	const handleMovieDetailsCardClick = () => {
+		navigate(`/details`, { state: { movie } });
+	}
+
+	const backdropPath = "https://image.tmdb.org/t/p/w500" + backdrop_url;
+
+	// TODO: get setted to watchlist in searchContent and provide here data
+	// TODO: add tags added recently, new season, etc.
+
 	return (
-		<div className="max-w-lg rounded overflow-hidden relative cursor-pointer group">
-			<div className="relative w-full">
+		<Card className="relative cursor-pointer group" onClick={() => handleMovieDetailsCardClick()}>
+			<CardHeader className="relative w-full p-0">
 				<img
-					alt="Movie Poster"
+					alt={title}
 					className="w-full"
-					src="https://sparrow.tortuga.wtf/content/stream/films/shadow_master_2022_bdremux_1080p_93172/screen.jpg"
+					src={backdropPath}
 				/>
 				<PlayIcon className="absolute w-12 h-12 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100" />
 				<div className="absolute flex items-center justify-center z-20 bottom-0 left-0 right-0">
@@ -317,25 +333,26 @@ const MovieDetailsCard = ({
 				<div className="absolute top-0 right-0 font-semibold text-foreground px-4 py-2">
 					{duration ? formatDuration(duration) : seasons}
 				</div>
-			</div>
-			<div className="px-6 py-4 flex flex-row justify-between bg-zinc-900">
+			</CardHeader>
+			<CardContent className="px-6 py-4 flex flex-row justify-between bg-zinc-900">
 				<div className="flex flex-col gap-2">
-					<h4 className="font-semibold text-sm text-green-400">Співпадіння: 98%</h4>
+					<h4 className="font-semibold text-sm text-green-400">Співпадіння: {matchPercentage}%</h4>
 					<div className="flex flex-row gap-2 items-center">
 						<AgeRestriction
-							data={{ age: "16" }}
+							data={{ age: ageRating }}
 						/>
-						<p className="text-sm">{2023}</p>
+						<p className="text-sm">{releaseYear}</p>
 					</div>
 				</div>
 				<div className="flex flex-col items-center">
-					<AddToWatchListButton className="w-8 h-8" isAdded={false} movie_id={1} />
+					<AddToWatchListButton className="w-8 h-8" isAdded={isAdded} movie_id={movie_id} />
 				</div>
-			</div>
-			<div className="px-6 pb-2 bg-zinc-900">
+			</CardContent>
+			<CardFooter className="px-6 pb-2 bg-zinc-900">
+				<p className="text-sm font-bold">{title}</p>
 				<p className="text-sm">{description}</p>
-			</div>
-		</div>
+			</CardFooter>
+		</Card>
 	);
 };
 
